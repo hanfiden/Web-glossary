cc0615b7e271e9f1cec9dd825482a53d90da90f7
Han Fiden <fidendu2@gmail.com>
2021-11-25 19:29:56 +0100

    add force redirect to www in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, only: :sessions
   root to: 'keywords#index'
   resources :keywords, except: [:show]
+
+  constraints(host: /^(?!www\.)/i) do
+    match '(*any)' => redirect { |params, request|
+      URI.parse(request.url).tap { |uri| uri.host = "www.#{uri.host}" }.to_s
+    }
+  end
 end
