1701520935235a129cb066a00772b996976375d0
Han Fiden <fidendu2@gmail.com>
2021-11-28 18:57:20 +0100

    remove achor of scroll up and down, add new function to js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,12 +18,14 @@ document.addEventListener('turbolinks:load', () => {
   function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     if (this.scrollY >= 560) scrollUp.classList.add('show-scrollup'); else scrollUp.classList.remove('show-scrollup')
+    scrollUp.onclick = function () { window.scrollTo(0, 0); };
   }
   window.addEventListener('scroll', scrollUp)
 
   function scrollDown() {
     const scrollDown = document.getElementById('scroll-down');
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) scrollDown.classList.remove('show-scrolldown'); else scrollDown.classList.add('show-scrolldown')
+    scrollDown.onclick = function () { window.scrollTo(0, document.body.scrollHeight); };
   }
   window.addEventListener('scroll', scrollDown)
 });
