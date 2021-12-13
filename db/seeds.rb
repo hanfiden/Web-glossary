4055ba0837e83bfcdfbe6ef3addf98c1ac3aaabf
Han Fiden <fidendu2@gmail.com>
2021-12-13 23:22:31 +0100

    comment seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
-puts "Cleaning database..."
+# puts "Cleaning database..."
 
-User.destroy_all
+# User.destroy_all
+# Keyword.destroy_all
 
-admin = User.new(email: 'fidenhan@gmail.com', password: "a9ght8")
-admin.save!
+# admin = User.new(email: 'user@example.com', password: "secret")
+# admin.save!
+
+# html = Keyword.new(title: 'html',
+# image_url: '1B3lzKgEpe4C8TmZFdSN90_3o-E062TJr',
+# definition: 'Hypertext Markup Language, a standardized system for tagging text files to achieve font,
+# colour, graphic, and hyperlink effects on World Wide Web pages.',
+# source_url: 'https://en.wikipedia.org/wiki/HTML')
+# html.save!
