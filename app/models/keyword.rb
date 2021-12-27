acf928cd8b04ae46f605844fe5567d69b353fb8c
Han Fiden <fidendu2@gmail.com>
2021-12-27 22:26:00 +0100

    add filter image_url to keyword model

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,8 @@
 class Keyword < ApplicationRecord
   validates :title, presence: true
   validates :definition, presence: true, length: { maximum: 250 }
+
+  def image_url=(value)
+    write_attribute(:image_url, value.gsub('https://drive.google.com/file/d/', '').gsub('/view?usp=sharing',''))
+  end
 end
